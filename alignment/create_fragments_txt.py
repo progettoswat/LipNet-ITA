53c4412023889ffb2ce76a5c668e60ec98c89aec
ProgettoSwat <progettoswathaga2@gmail.com>
2024-03-06 17:18:53 +0100

    added phrases for training with 56 phrases

--- alignment/create_fragments_txt.py
+++ alignment/create_fragments_txt.py
@@ -8,17 +8,54 @@ phrases = ['Salve quanto costa quell articolo',
            'Le auguro una buona giornata',
            'Buongiorno io sono Mario',
            'Buonasera io sono Mario',
-           'Piacere Luigi come stai',
+           'Piacere luigi come stai',
            'Tutto bene tu',
            'Tutto bene grazie',
            'Prendiamo un caffe al bar',
-           'Certo volentieri, io lo prendero macchiato',
+           'Certo volentieri io lo prendero macchiato',
            'A che ora arriva il pullman',
            'Dovrebbe arrivare tra qualche minuto',
            'Quanto costa il biglietto',
            'Purtroppo non lo so pero potresti chiedere all autista',
            'Va bene grazie lo stesso',
-           'Prego'
+           'Prego',
+           'Buongiorno a che ora inizia lo spettacolo',
+           'Lo spettacolo inizia a mezzanotte',
+           'Dove posso acquistare i biglietti',
+           'Puoi acquistare i biglietti online o al botteghino',
+           'Salve posso prendere il suo ordine',
+           'Vorrei una pizza margherita per favore',
+           'Vuole qualcosa da bere',
+           'Una coca cola grazie',
+           'Quanto tempo ci vorra per preparare la pizza',
+           'Circa venti minuti di attesa',
+           'Va bene aspettero',
+           'Ecco la sua pizza margherita buon appetito',
+           'Qual e la tua esperienza nel settore',
+           'Ho lavorato per due anni come consulente informatico',
+           'Perche vuoi lavorare qui',
+           'Ammiro la vostra cultura aziendale',
+           'Cosa hai fatto di interessante ultimamente',
+           'Ho fatto una gita in montagna',
+           'Hai qualche piano per il fine settimana',
+           'Penso di andare al concerto',
+           'Hai visto l ultimo film uscito',
+           'No non ancora Vogliamo andare insieme',
+           'Si volentieri',
+           'Che bello fare una passeggiata qui',
+           'Si l aria fresca fa bene',
+           'Vogliamo sederci su quella panchina',
+           'Si sembra un bel posto per riposare',
+           'Guarda quei bambini giocare con il pallone',
+           'Si sembrano divertirsi un sacco',
+           'Dobbiamo portare dell acqua la prossima volta',
+           'Hai ragione fa davvero caldo oggi',
+           'Qual e il numero dell autobus per l aereoporto',
+           'L autobus e il numero trentacinque',
+           'Dove stai andando di bello',
+           'Sto partendo per gli Stati Uniti',
+           'Se avessi piu soldi partirei anche io',
+           'Magari potremmo andare insieme in futuro'
            ]
 
 
@@ -33,16 +70,16 @@ def write_file(file, index):
 
 if __name__ == '__main__':
     main_path = input("Enter dataset path:")
-    main_path = main_path+"/"
+    main_path = main_path + "/"
     os.chdir(main_path)
 
     for dir in os.listdir(os.getcwd()):
         # dir s*
         os.chdir(dir)
-        print("\nFOLDER: "+dir)
+        print("\nFOLDER: " + dir)
         for video in os.listdir(os.getcwd()):
             if ".txt" not in video:
-                print("-->PROCESSING: "+video)
+                print("-->PROCESSING: " + video)
                 index = ""
                 dash = video.rfind("-")
 
